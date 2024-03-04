30ea4fd0603e5537a7250290def8fd51c07d86b9
Saif Ali <saifalishah2501@gmail.com>
2024-03-04 00:59:27 -0800

    addd

--- MyProsject/src/Router/Router.jsx
+++ MyProsject/src/Router/Router.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from '../ui/Component/Header/Header'
-import Home from '../ui/Page/Home'
+// import Home from '../ui/Page/Home'
 import Sungless from '../ui/Page/Sungless'
 import Men from '../ui/Page/Men'
 import Women from '../ui/Page/Women'
 import Unisex from '../ui/Page/Unisex'
 // import PrimCombos from '../ui/Page/PrimCombos'
 import Footer from '../ui/Component/Footer/Footer'
-import Body from '../ui/Page/Home/Body'
-import ExpolerPage from '../ui/Page/ExpolerPage/ExpolerPage'
+// import Body from '../ui/Page/Home/Body'
+// import ExpolerPage from '../ui/Page/ExpolerPage/ExpolerPage'
 import LogingData from '../ui/Page/LogingData'
+import MainPages from '../ui/Page/MainPage/MainPages'
+// import MainPages from '../ui/Page/MainPage/MainPages'
 // import { Home } from 'react-feather'
 
 export default function Router() {
@@ -18,11 +20,10 @@ export default function Router() {
     <div>
       <BrowserRouter>
       <Header/>
-      <Body/>
-      <ExpolerPage/>
-     
+      
+     {/* <MainPages/> */}
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path='/' element={<MainPages />}/>
         <Route path='/sungless' element={<Sungless/>}/>
         <Route path='/Men' element={<Men/>}/>
         <Route path='women' element={<Women/>}/>
